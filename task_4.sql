a3d738eae0f826ffe74a67252c5c2e9667ea75c8
Ariyo <ariyoaribass68@gmail.com>
2025-02-02 21:03:37 +0100

    My First full Description in MySQL server

--- task_4.sql
+++ task_4.sql
@@ -3,4 +3,5 @@ USE alx_book_store;
 -- Get full description of the 'Books' table without using DESCRIBE or EXPLAIN
 SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY 
 FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'books';
