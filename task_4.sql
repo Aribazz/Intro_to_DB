a33f04572debb9c528d4d6dc20845103cc846416
Ariyo <ariyoaribass68@gmail.com>
2025-02-02 20:15:49 +0100

    My First database with your very first data

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,5 @@
+-- Use the alx_book_store database
+USE alx_book_store;
 -- Get full description of the 'Books' table without using DESCRIBE or EXPLAIN
 SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY 
 FROM INFORMATION_SCHEMA.COLUMNS 
